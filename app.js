b6b38b7427130f03f90f67373f1738d5951b8c65
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-10-30 08:11:10 +0700

    add nav show/hide on icon click functionality

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var openIcon = document.querySelector("#menu-open-icon");
 var closeIcon = document.querySelector("#menu-close-icon");
+var nav = document.querySelector("#nav-flex-container");
 
 openIcon.addEventListener("click", () => {
     openIcon.classList.toggle("open");
     closeIcon.classList.toggle("open");
+    nav.classList.toggle("nav-open");
 });
 
 closeIcon.addEventListener("click", () => {
     openIcon.classList.toggle("open");
     closeIcon.classList.toggle("open");
+    nav.classList.toggle("nav-open");
 });
 
 
