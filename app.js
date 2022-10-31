cd200fa8a63fc48e1d88853ba808663a15798ace
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-10-31 09:18:11 +0700

    add slide down / slide up hamburger menu functionality

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 var openIcon = document.querySelector("#menu-open-icon");
 var closeIcon = document.querySelector("#menu-close-icon");
 var nav = document.querySelector("#nav-flex-container");
+var navUl = document.querySelector("#nav-ul");
+var navLi = document.querySelector(".nav-li")
+
 
 openIcon.addEventListener("click", () => {
     openIcon.classList.toggle("open");
     closeIcon.classList.toggle("open");
     nav.classList.toggle("nav-open");
+    navUl.classList.toggle("nav-ul-open");
+    navLi.classList.toggle("nav-li-open");
 });
 
 closeIcon.addEventListener("click", () => {
     openIcon.classList.toggle("open");
     closeIcon.classList.toggle("open");
     nav.classList.toggle("nav-open");
+    navUl.classList.toggle("nav-ul-open");
+    navLi.classList.toggle("nav-li-open");
 });
+
+
 
 
