51d682753a77fae8188f402bcb22da889be70a2d
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-10-31 09:51:18 +0700

    implement li expanding/collapsing animation on hamburger menu open/close

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var openIcon = document.querySelector("#menu-open-icon");
 var closeIcon = document.querySelector("#menu-close-icon");
 var nav = document.querySelector("#nav-flex-container");
 var navUl = document.querySelector("#nav-ul");
-var navLi = document.querySelector(".nav-li")
+var navLi = document.querySelectorAll(".nav-li")
+
+
 
 
 openIcon.addEventListener("click", () => {
@@ -10,7 +12,9 @@ openIcon.addEventListener("click", () => {
     closeIcon.classList.toggle("open");
     nav.classList.toggle("nav-open");
     navUl.classList.toggle("nav-ul-open");
-    navLi.classList.toggle("nav-li-open");
+    navLi.forEach(item => {
+        item.classList.toggle("nav-li-open");
+    })
 });
 
 closeIcon.addEventListener("click", () => {
@@ -18,7 +22,9 @@ closeIcon.addEventListener("click", () => {
     closeIcon.classList.toggle("open");
     nav.classList.toggle("nav-open");
     navUl.classList.toggle("nav-ul-open");
-    navLi.classList.toggle("nav-li-open");
+    navLi.forEach(item => {
+        item.classList.toggle("nav-li-open");
+    })
 });
 
 
