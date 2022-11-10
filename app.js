5d6d998623a3b49acf48c32c5cc30301a59aad1d
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-11-10 15:22:57 +0700

    add laptop media query to prevent nav from openeing when screen is larger than laptop

--- app.js
+++ app.js
@@ -6,36 +6,35 @@ var navLi = document.querySelectorAll(".nav-li");
 var navLiLink = document.querySelectorAll(".nav-li-link");
 
 
-
 /* open hamburger menu when open icon clicked */
-openIcon.addEventListener("click", () => {
-    openIcon.classList.toggle("open");
-    closeIcon.classList.toggle("open");
-    nav.classList.toggle("nav-open");
-    navUl.classList.toggle("nav-ul-open");
-    navLi.forEach(item => {
-        item.classList.toggle("nav-li-open");
-    });
-});
-
-/* close hamburger menu when close icon clicked */
-closeIcon.addEventListener("click", () => {
-    openIcon.classList.toggle("open");
-    closeIcon.classList.toggle("open");
-    nav.classList.toggle("nav-open");
-    navUl.classList.toggle("nav-ul-open");
-    navLi.forEach(item => item.classList.toggle("nav-li-open"));
-});
-
-
-/* close menu when nav li clicked */
-navLi.forEach(item => {
-    item.addEventListener("click", () => {
+    openIcon.addEventListener("click", () => {
         openIcon.classList.toggle("open");
         closeIcon.classList.toggle("open");
-        navUl.classList.toggle("nav-ul-open");
         nav.classList.toggle("nav-open");
-        navLi.forEach(li => li.classList.toggle("nav-li-open"));
+        navUl.classList.toggle("nav-ul-open");
+        navLi.forEach(item => {
+            item.classList.toggle("nav-li-open");
+        });
     });
-});
+
+    /* close hamburger menu when close icon clicked */
+    closeIcon.addEventListener("click", () => {
+        openIcon.classList.toggle("open");
+        closeIcon.classList.toggle("open");
+        nav.classList.toggle("nav-open");
+        navUl.classList.toggle("nav-ul-open");
+        navLi.forEach(item => item.classList.toggle("nav-li-open"));
+    });
+
+
+    /* close menu when nav li clicked */
+    navLi.forEach(item => {
+        item.addEventListener("click", () => {
+            openIcon.classList.toggle("open");
+            closeIcon.classList.toggle("open");
+            navUl.classList.toggle("nav-ul-open");
+            nav.classList.toggle("nav-open");
+            navLi.forEach(li => li.classList.toggle("nav-li-open"));
+        });
+    });
 
